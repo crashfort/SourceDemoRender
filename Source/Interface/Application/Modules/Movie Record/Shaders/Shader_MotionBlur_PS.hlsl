514bb0204d11bd2d8f333a5ccfd85978b645da4e
crashfort <crashfort@users.noreply.github.com>
2017-06-06 07:14:08 +0200

    Set up basic pixel shader

--- Source/Interface/Application/Modules/Movie Record/Shaders/Shader_MotionBlur_PS.hlsl
+++ Source/Interface/Application/Modules/Movie Record/Shaders/Shader_MotionBlur_PS.hlsl
@@ -1,6 +1,14 @@
+SamplerState TextureSampler
+{
+    Filter = MIN_MAG_MIP_LINEAR;
+    AddressU = Wrap;
+    AddressV = Wrap;
+};
+
 struct PS_INPUT
 {
-    float2 UV : TEXCOORD0;
+    float4 Pos : SV_Position;
+    float2 Tex : TEXCOORD0;
 };
 
 struct PS_OUTPUT
@@ -8,11 +16,13 @@ struct PS_OUTPUT
     float4 Color : SV_Target0;
 };
 
+Texture2D NewSample : register(t0);
+
 PS_OUTPUT PSMain(PS_INPUT input)
 {
     PS_OUTPUT ret;
 
-    ret.Color = float4(1, 1, 0, 1);
+    ret.Color = float4(NewSample.Sample(TextureSampler, input.Tex).rgb, 1.0);
 
     return ret;
 }
