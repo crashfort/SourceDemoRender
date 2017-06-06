e98cf92addabfb7f122d6686bd8e7828a8773fdd
crashfort <crashfort@users.noreply.github.com>
2017-06-06 17:52:34 +0200

    Rename shader texture variable

--- Source/Interface/Application/Modules/Movie Record/Shaders/Shader_MotionBlur_PS.hlsl
+++ Source/Interface/Application/Modules/Movie Record/Shaders/Shader_MotionBlur_PS.hlsl
@@ -16,13 +16,13 @@ struct PS_OUTPUT
     float4 Color : SV_Target0;
 };
 
-Texture2D NewSample : register(t0);
+Texture2D NewTexture : register(t0);
 
 PS_OUTPUT PSMain(PS_INPUT input)
 {
     PS_OUTPUT ret;
 
-    ret.Color = float4(NewSample.Sample(TextureSampler, input.Tex).rgb, 1.0);
+    ret.Color = float4(NewTexture.Sample(TextureSampler, input.Tex).rgb, 1.0);
 
     return ret;
 }
