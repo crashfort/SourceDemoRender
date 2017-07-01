c4f791a0fb831a55bf83126f21339fd3e2b79e67
crashfort <crashfort@users.noreply.github.com>
2017-07-01 15:25:56 +0200

    Add function to reset shader resources

--- Source/Interface/Application/Modules/Movie Record/MovieRecord.cpp
+++ Source/Interface/Application/Modules/Movie Record/MovieRecord.cpp
@@ -1495,6 +1495,39 @@ namespace
 					else
 					{
 						GPUFrameBuffer = std::make_unique<RGBAFrameBuffer>();
+				void ResetShaderInputs(ID3D11DeviceContext* context)
+				{
+					std::initializer_list<ID3D11ShaderResourceView*> srvs = 
+					{
+						nullptr,
+						nullptr,
+						nullptr,
+						nullptr,
+						nullptr,
+					};
+
+					std::initializer_list<ID3D11UnorderedAccessView*> uavs =
+					{
+						nullptr,
+						nullptr,
+						nullptr,
+						nullptr,
+						nullptr,
+					};
+
+					std::initializer_list<ID3D11Buffer*> cbufs =
+					{
+						nullptr,
+						nullptr,
+						nullptr,
+						nullptr,
+						nullptr,
+					};
+
+					context->CSSetShaderResources(0, srvs.size(), srvs.begin());
+					context->CSSetUnorderedAccessViews(0, uavs.size(), uavs.begin(), nullptr);
+					context->CSSetConstantBuffers(0, cbufs.size(), cbufs.begin());
+				}
 					}
 
 					GPUFrameBuffer->Create(device, reference);
