a1fb61cf4f6e427fd20cc99fa6abb527368976ed
crashfort <crashfort@users.noreply.github.com>
2017-08-02 14:57:52 +0200

    Use Shared Items Project instead of static lib

--- Projects/Plugin/Main/Precompiled Header/PrecompiledHeader.hpp
+++ Projects/Plugin/Main/Precompiled Header/PrecompiledHeader.hpp
@@ -1,57 +1,5 @@
 #pragma once
-
-#define WINVER 0x0601
-#define _WIN32_WINNT 0x0601
-#include "TargetVersion.hpp"
-
-#define WIN32_LEAN_AND_MEAN
-#define VC_EXTRALEAN
-
-/*
-	Order like in Windows.h.
-*/
-
-#define NOGDICAPMASKS
-#define NOVIRTUALKEYCODES
-//#define NOWINMESSAGES
-#define NOWINSTYLES
-#define NOSYSMETRICS
-#define NOMENUS
-#define NOICONS
-#define NOKEYSTATES
-#define NOSYSCOMMANDS
-#define NORASTEROPS
-#define NOSHOWWINDOW
-#define OEMRESOURCE
-#define NOATOM
-#define NOCLIPBOARD
-#define NOCOLOR
-//#define NOCTLMGR
-#define NODRAWTEXT
-//#define NOGDI
-#define NOKERNEL
-//#define NOUSER
-#define NONLS
-#define NOMB
-#define NOMEMMGR
-#define NOMETAFILE
-#define NOMINMAX
-//#define NOMSG
-#define NOOPENFILE
-#define NOSCROLL
-#define NOSERVICE
-#define NOSOUND
-#define NOTEXTMETRIC
-#define NOWH
-#define NOWINOFFSETS
-#define NOCOMM
-#define NOKANJI
-#define NOHELP
-#define NOPROFILER
-#define NODEFERWINDOWPOS
-#define NOMCX
-
-#include <Windows.h>
+#include "SDR Shared\BareWindows.hpp"
 #include <Psapi.h>
 #include <Shlwapi.h>
 #include <comdef.h>
