72b83e04d95560863346a9a5b31a555a321711d4
crashfort <crashfort@users.noreply.github.com>
2017-08-26 13:32:22 +0200

    Add UTF8 converters

--- Projects/Shared/Include/SDR Shared/String.hpp
+++ Projects/Shared/Include/SDR Shared/String.hpp
@@ -1,10 +1,35 @@
 #pragma once
 #include <string>
+#include <codecvt>
 
 using namespace std::string_literals;
 
 namespace SDR::String
 {
+	inline std::string ToUTF8(const std::wstring& input)
+	{
+		static std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+		return converter.to_bytes(input);
+	}
+
+	inline std::string ToUTF8(const wchar_t* input)
+	{
+		static std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+		return converter.to_bytes(input);
+	}
+
+	inline std::wstring FromUTF8(const std::string& input)
+	{
+		static std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+		return converter.from_bytes(input);
+	}
+
+	inline std::wstring FromUTF8(const char* input)
+	{
+		static 	std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+		return converter.from_bytes(input);
+	}
+
 	template <typename... Args>
 	inline std::string GetFormattedString(const char* format, Args&&... args)
 	{
