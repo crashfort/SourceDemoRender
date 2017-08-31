bf866edaf36aeeec483ac52eeb51b4bd8c0417df
crashfort <crashfort@users.noreply.github.com>
2017-08-31 20:19:37 +0200

    Add more json helpers

--- Projects/Shared/Include/SDR Shared/Json.hpp
+++ Projects/Shared/Include/SDR Shared/Json.hpp
@@ -20,4 +20,40 @@ namespace SDR::Json
 	{
 		return FromFile(name.c_str());
 	}
+
+	inline auto GetIterator(const rapidjson::Value& value, const char* name)
+	{
+		auto it = value.FindMember(name);
+
+		if (it == value.MemberEnd())
+		{
+			SDR::Error::Make("Member Json value \"%s\" not found", name);
+		}
+
+		return it;
+	}
+
+	inline auto GetInt(const rapidjson::Value& value, const char* name)
+	{
+		auto it = GetIterator(value, name);
+
+		if (!it->value.IsInt())
+		{
+			SDR::Error::Make("Member Json value \"%s\" not an int", name);
+		}
+
+		return it->value.GetInt();
+	}
+
+	inline auto GetString(const rapidjson::Value& value, const char* name)
+	{
+		auto it = GetIterator(value, name);
+
+		if (!it->value.IsString())
+		{
+			SDR::Error::Make("Member Json value \"%s\" not a string", name);
+		}
+
+		return it->value.GetString();
+	}
 }
