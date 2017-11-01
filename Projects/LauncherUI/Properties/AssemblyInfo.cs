87719725f92b685d410103663740a6427825b2e0
crashfort <crashfort@users.noreply.github.com>
2017-11-01 20:27:39 +0100

    Update LauncherUI project files for new output

--- Projects/LauncherUI/Properties/AssemblyInfo.cs
+++ Projects/LauncherUI/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LauncherUI")]
+[assembly: AssemblyTitle("SDR Launcher UI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LauncherUI")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
