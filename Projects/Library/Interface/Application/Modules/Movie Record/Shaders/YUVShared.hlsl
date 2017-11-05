f7feb0803bed0f33c94ebe2e28afebd8ab3ba5c5
crashfort <crashfort@users.noreply.github.com>
2017-11-05 19:22:32 +0100

    Revert YUV shader structure changes
    Produced wrong colors for some people.

--- Projects/Library/Interface/Application/Modules/Movie Record/Shaders/YUVShared.hlsl
+++ Projects/Library/Interface/Application/Modules/Movie Record/Shaders/YUVShared.hlsl
@@ -7,24 +7,29 @@ RWBuffer<uint> ChannelV : register(u2);
 
 cbuffer YUVInputData : register(b1)
 {
-	int4 Strides;
-	float3x3 Coeffs;
+	int3 Strides;
+	int Padding1;
+	float3 CoeffY;
+	int Padding2;
+	float3 CoeffU;
+	int Padding3;
+	float3 CoeffV;
 };
 
 float Y_Full(float3 pix)
 {
 	pix *= 255.0;
-	return 0 + (pix.r * Coeffs[0][0]) + (pix.g * Coeffs[0][1]) + (pix.b * Coeffs[0][2]);
+	return 0 + (pix.r * CoeffY[0]) + (pix.g * CoeffY[1]) + (pix.b * CoeffY[2]);
 }
 
 float U_Full(float3 pix)
 {
 	pix *= 255.0;
-	return 128 + (pix.r * Coeffs[1][0]) + (pix.g * Coeffs[1][1]) + (pix.b * Coeffs[1][2]);
+	return 128 + (pix.r * CoeffU[0]) + (pix.g * CoeffU[1]) + (pix.b * CoeffU[2]);
 }
 
 float V_Full(float3 pix)
 {
 	pix *= 255.0;
-	return 128 + (pix.r * Coeffs[2][0]) + (pix.g * Coeffs[2][1]) + (pix.b * Coeffs[2][2]);
+	return 128 + (pix.r * CoeffV[0]) + (pix.g * CoeffV[1]) + (pix.b * CoeffV[2]);
 }
