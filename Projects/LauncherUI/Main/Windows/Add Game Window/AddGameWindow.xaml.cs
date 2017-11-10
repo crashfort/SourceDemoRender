9663f9a35f5bba13798a5879b19266115e82ab1b
crashfort <crashfort@users.noreply.github.com>
2017-11-10 03:03:25 +0100

    Move game adding events to separate functions

--- Projects/LauncherUI/Main/Windows/Add Game Window/AddGameWindow.xaml.cs
+++ Projects/LauncherUI/Main/Windows/Add Game Window/AddGameWindow.xaml.cs
@@ -53,6 +53,97 @@ namespace LauncherUI
 		public List<GameData> ExistingGames;
 		public event EventHandler<GameData> OnGameAdded;
 
+		private void OnSDRDirectorySelected(string sdrpath)
+		{
+			ErrorText.Text = null;
+			OKButton.IsEnabled = false;
+			GameExeGrid.IsEnabled = false;
+			GameExeTextBox.Text = null;
+
+			SDRDirTextBox.Text = sdrpath;
+
+			try
+			{
+				if (sdrpath.Length == 0)
+				{
+					SDRDirTextBox.Focus();
+					throw new Exception("SDR path is empty.");
+				}
+
+				if (!System.IO.Directory.Exists(sdrpath))
+				{
+					SDRDirTextBox.Focus();
+					throw new Exception("Specified SDR path does not exist.");
+				}
+
+				var sdrpathinfo = new System.IO.DirectoryInfo(sdrpath);
+
+				if (sdrpathinfo.Name != "SDR")
+				{
+					SDRDirTextBox.Focus();
+					throw new Exception("Specified SDR path is not related to SDR.");
+				}
+
+				var files = new string[] { "SourceDemoRender.dll", "LauncherCLI.exe", "GameConfig.json" };
+
+				foreach (var name in files)
+				{
+					if (!System.IO.File.Exists(System.IO.Path.Combine(sdrpath, name)))
+					{
+						SDRDirTextBox.Focus();
+
+						var format = string.Format("File \"{0}\" does not exist in SDR folder.", name);
+						throw new Exception(format);
+					}
+				}
+
+				foreach (var game in ExistingGames)
+				{
+					if (game.SDRPath == sdrpath)
+					{
+						throw new Exception("Game is already added.");
+					}
+				}
+
+				var configpath = System.IO.Path.Combine(sdrpath, "GameConfig.json");
+				var content = System.IO.File.ReadAllText(configpath, System.Text.Encoding.UTF8);
+				var document = System.Json.JsonValue.Parse(content);
+				var parentdir = System.IO.Directory.GetParent(sdrpath);
+
+				if (!document.ContainsKey(parentdir.Name))
+				{
+					var format = string.Format("Game \"{0}\" does not exist in game config.", parentdir.Name);
+					throw new Exception(format);
+				}
+
+				var gamejson = document[parentdir.Name];
+
+				if (!gamejson.ContainsKey("DisplayName"))
+				{
+					var format = string.Format("Game config does not contain \"DisplayName\" member for game \"{0}\".", parentdir.Name);
+					throw new Exception(format);
+				}
+
+				CurrentVerifyGame.DisplayName = gamejson["DisplayName"];
+
+				if (!gamejson.ContainsKey("ExecutableName"))
+				{
+					var format = string.Format("Game config does not contain \"ExecutableName\" member for game \"{0}\".", CurrentVerifyGame.DisplayName);
+					throw new Exception(format);
+				}
+
+				CurrentVerifyGame.ExpectedExecutableName = gamejson["ExecutableName"];
+			}
+
+			catch (Exception error)
+			{
+				ErrorText.Text = error.Message;
+				return;
+			}
+
+			GameExeGrid.IsEnabled = true;
+		}
+
 		private void SDRBrowse_Click(object sender, RoutedEventArgs args)
 		{
 			var dialog = new CommonOpenFileDialog();
@@ -69,96 +160,47 @@ namespace LauncherUI
 
 			if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
 			{
-				ErrorText.Text = null;
-				OKButton.IsEnabled = false;
-				GameExeGrid.IsEnabled = false;
-				GameExeTextBox.Text = null;
-
-				SDRDirTextBox.Text = dialog.FileName;
-
-				string sdrpath = SDRDirTextBox.Text.Trim();
-
-				try
-				{
-					if (sdrpath.Length == 0)
-					{
-						SDRDirTextBox.Focus();
-						throw new Exception("SDR path is empty.");
-					}
-
-					if (!System.IO.Directory.Exists(sdrpath))
-					{
-						SDRDirTextBox.Focus();
-						throw new Exception("Specified SDR path does not exist.");
-					}
-
-					var sdrpathinfo = new System.IO.DirectoryInfo(sdrpath);
-
-					if (sdrpathinfo.Name != "SDR")
-					{
-						SDRDirTextBox.Focus();
-						throw new Exception("Specified SDR path is not related to SDR.");
-					}
-
-					var files = new string[] { "SourceDemoRender.dll", "LauncherCLI.exe", "GameConfig.json" };
-
-					foreach (var name in files)
-					{
-						if (!System.IO.File.Exists(System.IO.Path.Combine(sdrpath, name)))
-						{
-							SDRDirTextBox.Focus();
-
-							var format = string.Format("File \"{0}\" does not exist in SDR folder.", name);
-							throw new Exception(format);
-						}
-					}
-
-					foreach (var game in ExistingGames)
-					{
-						if (game.SDRPath == sdrpath)
-						{
-							throw new Exception("Game is already added.");
-						}
-					}
-
-					var configpath = System.IO.Path.Combine(sdrpath, "GameConfig.json");
-					var content = System.IO.File.ReadAllText(configpath, System.Text.Encoding.UTF8);
-					var document = System.Json.JsonValue.Parse(content);
-					var parentdir = System.IO.Directory.GetParent(sdrpath);
-
-					if (!document.ContainsKey(parentdir.Name))
-					{
-						var format = string.Format("Game \"{0}\" does not exist in game config.", parentdir.Name);
-						throw new Exception(format);
-					}
-
-					var gamejson = document[parentdir.Name];
-
-					if (!gamejson.ContainsKey("DisplayName"))
-					{
-						var format = string.Format("Game config does not contain \"DisplayName\" member for game \"{0}\".", parentdir.Name);
-						throw new Exception(format);
-					}
-
-					CurrentVerifyGame.DisplayName = gamejson["DisplayName"];
-
-					if (!gamejson.ContainsKey("ExecutableName"))
-					{
-						var format = string.Format("Game config does not contain \"ExecutableName\" member for game \"{0}\".", CurrentVerifyGame.DisplayName);
-						throw new Exception(format);
-					}
-
-					CurrentVerifyGame.ExpectedExecutableName = gamejson["ExecutableName"];
-				}
-
-				catch (Exception error)
-				{
-					ErrorText.Text = error.Message;
-					return;
-				}
-
-				GameExeGrid.IsEnabled = true;
+				OnSDRDirectorySelected(dialog.FileName.Trim());
 			}
+		}
+
+		private void OnGameExecutableSelected(string exepath)
+		{
+			ErrorText.Text = null;
+			OKButton.IsEnabled = false;
+
+			GameExeTextBox.Text = exepath;
+
+			try
+			{
+				if (exepath.Length == 0)
+				{
+					GameExeTextBox.Focus();
+					throw new Exception("Executable path is empty.");
+				}
+
+				if (!System.IO.File.Exists(exepath))
+				{
+					GameExeTextBox.Focus();
+					throw new Exception("Specified executable path does not exist.");
+				}
+
+				var fileinfo = new System.IO.FileInfo(exepath);
+
+				if (!fileinfo.FullName.EndsWith(CurrentVerifyGame.ExpectedExecutableName))
+				{
+					var format = string.Format("Executable name for \"{0}\" should be \"{1}\".", CurrentVerifyGame.DisplayName, CurrentVerifyGame.ExpectedExecutableName);
+					throw new Exception(format);
+				}
+			}
+
+			catch (Exception error)
+			{
+				ErrorText.Text = error.Message;
+				return;
+			}
+
+			OKButton.IsEnabled = true;
 		}
 
 		private void ExeBrowse_Click(object sender, RoutedEventArgs args)
@@ -174,43 +216,7 @@ namespace LauncherUI
 
 			if (res.Value)
 			{
-				ErrorText.Text = null;
-				OKButton.IsEnabled = false;
-
-				GameExeTextBox.Text = dialog.FileName;
-
-				var exepath = GameExeTextBox.Text.Trim();
-
-				try
-				{
-					if (exepath.Length == 0)
-					{
-						GameExeTextBox.Focus();
-						throw new Exception("Executable path is empty.");
-					}
-
-					if (!System.IO.File.Exists(exepath))
-					{
-						GameExeTextBox.Focus();
-						throw new Exception("Specified executable path does not exist.");
-					}
-
-					var fileinfo = new System.IO.FileInfo(exepath);
-
-					if (!fileinfo.FullName.EndsWith(CurrentVerifyGame.ExpectedExecutableName))
-					{
-						var format = string.Format("Executable name for \"{0}\" should be \"{1}\".", CurrentVerifyGame.DisplayName, CurrentVerifyGame.ExpectedExecutableName);
-						throw new Exception(format);
-					}
-				}
-
-				catch (Exception error)
-				{
-					ErrorText.Text = error.Message;
-					return;
-				}
-
-				OKButton.IsEnabled = true;
+				OnGameExecutableSelected(dialog.FileName.Trim());
 			}
 		}
 	}
