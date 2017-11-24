c0e9b6a0dcbf0eca5ed44b5939ddd46e9c8c4ff7
crashfort <crashfort@users.noreply.github.com>
2017-11-24 18:16:58 +0100

    Remove Launcher CLI script

--- Output/SDR/LauncherCLI User.bat
+++
@@ -1,9 +0,0 @@
-:: Insert your own parameters here.
-::
-:: First parameter is the game executable path, see the Github readme for help.
-:: Any parameter after that will be passed to the game, such as window mode, width and height.
-::
-:: Working example:
-:: .\LauncherCLI.exe /GAME "C:\Program Files (x86)\Steam\steamapps\common\Half-Life 2\hl2.exe" /PARAMS "-window -w 1280 -h 720 -novid -dev -nojoy"
-::
-.\LauncherCLI.exe 
