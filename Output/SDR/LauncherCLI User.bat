b7b5dbd62d08cb8f49375153f44817a07ea293d9
crashfort <crashfort@users.noreply.github.com>
2017-11-24 07:01:16 +0100

    Update batch file with new format

--- Output/SDR/LauncherCLI User.bat
+++ Output/SDR/LauncherCLI User.bat
@@ -4,6 +4,6 @@
 :: Any parameter after that will be passed to the game, such as window mode, width and height.
 ::
 :: Working example:
-:: .\LauncherCLI.exe "C:\Program Files (x86)\Steam\steamapps\common\Half-Life 2\hl2.exe" -window -w 1280 -h 720 -novid -dev -nojoy
+:: .\LauncherCLI.exe /GAME "C:\Program Files (x86)\Steam\steamapps\common\Half-Life 2\hl2.exe" /PARAMS "-window -w 1280 -h 720 -novid -dev -nojoy"
 ::
 .\LauncherCLI.exe 
