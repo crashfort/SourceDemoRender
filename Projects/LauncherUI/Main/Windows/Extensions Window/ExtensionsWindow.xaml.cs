48f9f0436cb46794b4e47c649c81676c15d473a3
crashfort <crashfort@users.noreply.github.com>
2017-12-19 12:17:24 +0100

    Allow extensions to be toggled in LauncherUI

--- Projects/LauncherUI/Main/Windows/Extensions Window/ExtensionsWindow.xaml.cs
+++ Projects/LauncherUI/Main/Windows/Extensions Window/ExtensionsWindow.xaml.cs
@@ -39,8 +39,11 @@ namespace LauncherUI
 
 		class ListBoxData
 		{
+			public bool Enabled;
+
 			public string Name;
 			public string FileName;
+			public string RelativePath;
 			public string Author;
 			public int Version;
 		}
@@ -56,9 +59,8 @@ namespace LauncherUI
 			SyncWithUI();
 		}
 
-		void PopulateList()
+		void LoadExtensionsFromPath(string path, bool enabled)
 		{
-			var path = System.IO.Path.Combine("Extensions\\Enabled\\");
 			var files = System.IO.Directory.GetFiles(path, "*.dll", System.IO.SearchOption.TopDirectoryOnly);
 
 			foreach (var file in files)
@@ -81,6 +83,9 @@ namespace LauncherUI
 						var fileinfo = new System.IO.FileInfo(file);
 
 						var data = new ListBoxData();
+						data.RelativePath = file;
+						data.Enabled = enabled;
+
 						data.Name = Marshal.PtrToStringAnsi(result.Name);
 						data.FileName = fileinfo.Name;
 						data.Author = Marshal.PtrToStringAnsi(result.Author);
@@ -97,6 +102,12 @@ namespace LauncherUI
 			}
 		}
 
+		void PopulateList()
+		{
+			LoadExtensionsFromPath("Extensions\\Enabled\\", true);
+			LoadExtensionsFromPath("Extensions\\Disabled\\", false);
+		}
+
 		void ResolveFromLocalOrder()
 		{
 			if (Extensions.Count < 2)
@@ -104,7 +115,7 @@ namespace LauncherUI
 				return;
 			}
 
-			var path = System.IO.Path.Combine("Extensions\\Enabled\\Order.json");
+			var path = "Extensions\\Enabled\\Order.json";
 
 			if (!System.IO.File.Exists(path))
 			{
@@ -132,12 +143,15 @@ namespace LauncherUI
 			{
 				foreach (var temp in copy)
 				{
-					if (item == temp.FileName)
+					if (temp.Enabled)
 					{
-						Extensions.Add(temp);
-						copy.Remove(temp);
+						if (item == temp.FileName)
+						{
+							Extensions.Add(temp);
+							copy.Remove(temp);
 
-						break;
+							break;
+						}
 					}
 				}
 			}
@@ -152,10 +166,53 @@ namespace LauncherUI
 		{
 			ExtensionsList.Items.Clear();
 
+			MoveUpButton.IsEnabled = false;
+			MoveTopButton.IsEnabled = false;
+
+			MoveDownButton.IsEnabled = false;
+			MoveBottomButton.IsEnabled = false;
+
 			foreach (var item in Extensions)
 			{
 				var content = new System.Windows.Controls.Grid();
-				content.Height = 65;
+				content.Height = 70;
+
+				var enabledseq = new System.Windows.Controls.TextBlock();
+				enabledseq.FontSize = 15;
+				enabledseq.HorizontalAlignment = HorizontalAlignment.Left;
+				enabledseq.VerticalAlignment = VerticalAlignment.Bottom;
+
+				var enabledtitle = new System.Windows.Controls.TextBlock();
+				enabledtitle.Text = "Enabled ";
+				enabledtitle.Foreground = System.Windows.Media.Brushes.Black;
+
+				var enabledstr = new System.Windows.Controls.TextBlock();
+				enabledstr.Text = item.FileName;
+				enabledstr.Foreground = System.Windows.Media.Brushes.Gray;
+
+				enabledseq.Inlines.Add(enabledtitle);
+				enabledseq.Inlines.Add(enabledstr);
+
+				var check = new System.Windows.Controls.CheckBox();
+				check.Content = enabledseq;
+				check.FontSize = 15;
+				check.HorizontalAlignment = HorizontalAlignment.Left;
+				check.VerticalAlignment = VerticalAlignment.Bottom;
+				check.Foreground = System.Windows.Media.Brushes.Gray;
+				check.VerticalContentAlignment = VerticalAlignment.Center;
+
+				check.IsChecked = item.Enabled;
+
+				check.DataContext = item;
+				check.Checked += ExtensionEnabledCheck_Checked;
+				check.Unchecked += ExtensionEnabledCheck_Unchecked;
+
+				var title = new System.Windows.Controls.TextBlock();
+				title.Text = item.Name;
+				title.FontSize = 30;
+				title.FontWeight = FontWeights.Thin;
+				title.HorizontalAlignment = HorizontalAlignment.Left;
+				title.VerticalAlignment = VerticalAlignment.Top;
 
 				var index = new System.Windows.Controls.TextBlock();
 				index.Text = string.Format("{0}#", ExtensionsList.Items.Count + 1);
@@ -166,27 +223,13 @@ namespace LauncherUI
 				index.VerticalAlignment = VerticalAlignment.Top;
 				index.FlowDirection = FlowDirection.RightToLeft;
 
-				var title = new System.Windows.Controls.TextBlock();
-				title.Text = item.Name;
-				title.FontSize = 30;
-				title.FontWeight = FontWeights.Thin;
-				title.HorizontalAlignment = HorizontalAlignment.Left;
-				title.VerticalAlignment = VerticalAlignment.Top;
-
-				var file = new System.Windows.Controls.TextBlock();
-				file.Text = item.FileName;
-				file.FontSize = 15;
-				file.Foreground = System.Windows.Media.Brushes.Gray;
-				file.HorizontalAlignment = HorizontalAlignment.Left;
-				file.VerticalAlignment = VerticalAlignment.Bottom;
-
 				var infostr = new System.Windows.Controls.TextBlock();
 				infostr.FontSize = 15;
 				infostr.HorizontalAlignment = HorizontalAlignment.Right;
 				infostr.VerticalAlignment = VerticalAlignment.Bottom;
 
 				var authortitle = new System.Windows.Controls.TextBlock();
-				authortitle.Text = "Author: ";
+				authortitle.Text = "Author ";
 				authortitle.Foreground = System.Windows.Media.Brushes.Black;
 
 				var authorstr = new System.Windows.Controls.TextBlock();
@@ -194,7 +237,7 @@ namespace LauncherUI
 				authorstr.Foreground = System.Windows.Media.Brushes.Gray;
 
 				var versiontitle = new System.Windows.Controls.TextBlock();
-				versiontitle.Text = " Version: ";
+				versiontitle.Text = " Version ";
 				versiontitle.Foreground = System.Windows.Media.Brushes.Black;
 
 				var versionstr = new System.Windows.Controls.TextBlock();
@@ -206,13 +249,29 @@ namespace LauncherUI
 				infostr.Inlines.Add(versiontitle);
 				infostr.Inlines.Add(versionstr);
 
+				content.Children.Add(check);
 				content.Children.Add(title);
-				content.Children.Add(file);
 				content.Children.Add(infostr);
 				content.Children.Add(index);
 
 				ExtensionsList.Items.Add(content);
 			}
+		}
+
+		void ExtensionEnabledCheck_Checked(object sender, RoutedEventArgs args)
+		{
+			var control = sender as System.Windows.Controls.CheckBox;
+			var data = (ListBoxData)control.DataContext;
+
+			data.Enabled = true;
+		}
+
+		void ExtensionEnabledCheck_Unchecked(object sender, RoutedEventArgs args)
+		{
+			var control = sender as System.Windows.Controls.CheckBox;
+			var data = (ListBoxData)control.DataContext;
+
+			data.Enabled = false;
 		}
 
 		void SwapItems(int index, int newindex)
@@ -230,6 +289,11 @@ namespace LauncherUI
 
 		void MoveUpButton_Click(object sender, RoutedEventArgs args)
 		{
+			if (Extensions.Count < 2)
+			{
+				return;
+			}
+
 			var index = ExtensionsList.SelectedIndex;
 			var newindex = index - 1;
 
@@ -244,6 +308,11 @@ namespace LauncherUI
 
 		void MoveDownButton_Click(object sender, RoutedEventArgs args)
 		{
+			if (Extensions.Count < 2)
+			{
+				return;
+			}
+
 			var index = ExtensionsList.SelectedIndex;
 			var newindex = index + 1;
 
@@ -258,6 +327,11 @@ namespace LauncherUI
 
 		void MoveTopButton_Click(object sender, RoutedEventArgs args)
 		{
+			if (Extensions.Count < 2)
+			{
+				return;
+			}
+
 			var index = ExtensionsList.SelectedIndex;
 			var newindex = 0;
 
@@ -272,6 +346,11 @@ namespace LauncherUI
 
 		void MoveBottomButton_Click(object sender, RoutedEventArgs args)
 		{
+			if (Extensions.Count < 2)
+			{
+				return;
+			}
+
 			var index = ExtensionsList.SelectedIndex;
 			var newindex = ExtensionsList.Items.Count - 1;
 
@@ -286,18 +365,40 @@ namespace LauncherUI
 
 		void OKButton_Click(object sender, RoutedEventArgs args)
 		{
-			var path = System.IO.Path.Combine("Extensions\\Enabled\\Order.json");
+			var enabledpath = "Extensions\\Enabled\\";
+			var disabledpath = "Extensions\\Disabled\\";
 
 			var saverestore = new List<string>();
 
 			foreach (var item in Extensions)
 			{
-				saverestore.Add(item.FileName);
+				if (item.Enabled)
+				{
+					saverestore.Add(item.FileName);
+
+					var newlocation = System.IO.Path.Combine(enabledpath, item.FileName);
+
+					if (item.RelativePath != newlocation)
+					{
+						System.IO.File.Move(item.RelativePath, newlocation);
+					}
+				}
+
+				else
+				{
+					var newlocation = System.IO.Path.Combine(disabledpath, item.FileName);
+
+					if (item.RelativePath != newlocation)
+					{
+						System.IO.File.Move(item.RelativePath, newlocation);
+					}
+				}
 			}
 
 			var json = Newtonsoft.Json.JsonConvert.SerializeObject(saverestore);
 
-			System.IO.File.WriteAllText(path, json, new System.Text.UTF8Encoding(false));
+			var orderpath = System.IO.Path.Combine(enabledpath, "Order.json");
+			System.IO.File.WriteAllText(orderpath, json, new System.Text.UTF8Encoding(false));
 
 			Close();
 		}
