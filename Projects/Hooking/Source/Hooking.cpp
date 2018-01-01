b85c593091eddb2c63c681fb9e83ecf68f67e543
crashfort <crashfort@users.noreply.github.com>
2018-01-01 20:17:06 +0100

    Correct off by one in SDR::Hooking error message

--- Projects/Hooking/Source/Hooking.cpp
+++ Projects/Hooking/Source/Hooking.cpp
@@ -356,7 +356,7 @@ void SDR::Hooking::WarnIfVariantOutOfBounds(int variant, int max)
 
 	if (variant < 0 || variant >= max)
 	{
-		SDR::Error::Make("Variant overload %d not in bounds (%d max)", variant, max);
+		SDR::Error::Make("Variant overload %d not in bounds (%d max)", variant, max - 1);
 	}
 }
 
