da917d872f3ade6f7b29cf550a8d4d6b0c24feec
crashfort <crashfort@users.noreply.github.com>
2019-05-29 20:00:17 +0200

    Build both new extensions in scripts

--- Projects/BuildExtensions.bat
+++ Projects/BuildExtensions.bat
@@ -2,3 +2,5 @@ set Configuration=Release
 msbuild "SampleExtension\SampleExtension.sln" /m:4 /t:Rebuild /clp:ErrorsOnly;WarningsOnly
 msbuild "PreviewWindowExtension\PreviewWindowExtension.sln" /m:4 /t:Rebuild /clp:ErrorsOnly;WarningsOnly
 msbuild "Direct2DContextExtension\Direct2DContextExtension.sln" /m:4 /t:Rebuild /clp:ErrorsOnly;WarningsOnly
+msbuild "VelocityTextExtension\VelocityTextExtension.sln" /m:4 /t:Rebuild /clp:ErrorsOnly;WarningsOnly
+msbuild "MultiProcessExtension\MultiProcessExtension.sln" /m:4 /t:Rebuild /clp:ErrorsOnly;WarningsOnly
