b32ad475b4660b44a2cdf8276c32f238ebd54aeb
crashfort <crashfort@users.noreply.github.com>
2021-07-25 18:59:17 +0200

    Remove everything

--- projects/svr_core/src/svr/graphics/d3d11/shaders/texture_ps.hlsl
+++
@@ -1,7 +0,0 @@
-Texture2D<float4> input_texture : register(t0);
-SamplerState input_texture_sampler : register(s0);
-
-float4 main(float2 coord : TEXCOORD0) : SV_TARGET0
-{
-    return input_texture.Sample(input_texture_sampler, coord);
-}
--- projects/svr_core/src/svr/platform/platform_windows.cpp
+++
@@ -1,9 +0,0 @@
-#include <svr/platform.hpp>
-
-namespace svr
-{
-    platform_type platform_current()
-    {
-        return PLATFORM_WINDOWS;
-    }
-}
