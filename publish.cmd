a583124c3e0de926fc227894acae157f33f77378
crashfort <crashfort@users.noreply.github.com>
2021-09-07 22:52:19 +0200

    Make publish cmd

--- publish.cmd
+++ publish.cmd
@@ -2,6 +2,20 @@
 
 REM We use zip because more often than not people don't have 7zip or equivalent. Windows has built in support for opening zip.
 
-del svr.zip
-7z a -bd -aoa -bb0 -tzip -y svr.zip bin\svr_game.dll bin\svr_launcher.exe bin\ffmpeg.exe bin\data\ update.cmd -xr!SVR_LOG.TXT
-7z rn svr.zip bin svr
+setlocal
+mkdir publish_temp\svr
+
+xcopy /Q /E bin\svr_game.dll publish_temp\svr\
+xcopy /Q /E bin\svr_launcher.exe publish_temp\svr\
+xcopy /Q /E bin\ffmpeg.exe publish_temp\svr\
+xcopy /Q /E bin\data\ publish_temp\svr\data\
+xcopy /Q /E update.cmd publish_temp\svr\
+del /S /Q publish_temp\svr\data\SVR_LOG.TXT
+
+cd publish_temp
+
+7z a -bd -aoa -bb0 -tzip -y svr.zip svr
+move /Y svr.zip ..\svr.zip
+
+cd ..
+rmdir /S /Q publish_temp
