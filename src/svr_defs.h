e002046409f32cdbe9e6bcfc1f70e391250f629b
crashfort <crashfort@users.noreply.github.com>
2021-10-07 23:16:51 +0200

    Set version and changelog

--- src/svr_defs.h
+++ src/svr_defs.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "svr_common.h"
 
-const s32 SVR_VERSION = 34;
+const s32 SVR_VERSION = 35;
 
 // Steam app ids of the games we support.
 const SteamAppId STEAM_GAME_CSS = 240;
