9f766f1c9eb60accce1b2476b3e6694d323285f7
SmileyAG <58108407+SmileyAG@users.noreply.github.com>
2021-10-12 16:37:55 +0400

    Add HL2, BMS, ZPS

--- src/svr_defs.h
+++ src/svr_defs.h
@@ -4,6 +4,9 @@
 const s32 SVR_VERSION = 35;
 
 // Steam app ids of the games we support.
+const SteamAppId STEAM_GAME_HL2 = 220;
 const SteamAppId STEAM_GAME_CSS = 240;
 const SteamAppId STEAM_GAME_TF2 = 440;
 const SteamAppId STEAM_GAME_CSGO = 730;
+const SteamAppId STEAM_GAME_ZPS = 17500;
+const SteamAppId STEAM_GAME_BMS = 362890;
