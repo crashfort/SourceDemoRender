4fde17f54ece7885e92785f88b193e596c64e20d
crashfort <crashfort@users.noreply.github.com>
2022-08-12 14:16:12 +0200

    Update version

--- src/svr_defs.h
+++ src/svr_defs.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "svr_common.h"
 
-const s32 SVR_VERSION = 38;
+const s32 SVR_VERSION = 40;
 
 // Steam app ids of the games we support.
 const SteamAppId STEAM_GAME_HL2 = 220;
