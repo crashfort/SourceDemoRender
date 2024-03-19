f03cd1e49c39c82b2d8baa0b5a2e0f4769f3cc3a
crashfort <crashfort@users.noreply.github.com>
2024-03-19 21:50:29 +0100

    Process motion blur in linear color space

--- shaders/downsample.hlsl
+++ shaders/downsample.hlsl
@@ -3,11 +3,16 @@
 Texture2D<float4> source_texture : register(t0);
 RWTexture2D<unorm float4> dest_texture : register(u0);
 
+float4 from_linear(float4 v)
+{
+    return pow(max(v, 0.0f), 1.0f / 2.2f);
+}
+
 // This must be synchronized with the compute shader Dispatch call in CPU code!
 [numthreads(8, 8, 1)]
 void main(uint3 dtid : SV_DispatchThreadID)
 {
     uint2 pos = dtid.xy;
-    float4 source_pix = source_texture.Load(dtid);
+    float4 source_pix = from_linear(source_texture.Load(dtid)); // Must go back from linear space used in mosample.
     dest_texture[pos] = source_pix;
 }
--- shaders/motion_sample.hlsl
+++ shaders/motion_sample.hlsl
@@ -6,10 +6,17 @@ cbuffer mosample_buffer_0 : register(b0)
 Texture2D<unorm float4> source_texture : register(t0);
 RWTexture2D<float4> dest_texture : register(u0);
 
+float4 to_linear(float4 v)
+{
+    return pow(max(v, 0.0f), 2.2f);
+}
+
 // This must be synchronized with the compute shader Dispatch call in CPU code!
 [numthreads(8, 8, 1)]
 void main(uint3 dtid : SV_DispatchThreadID)
 {
-    float4 source_pix = source_texture.Load(dtid);
-    dest_texture[dtid.xy] += source_pix * mosample_weight;
+    // Must be blending in linear space! The mosample texture is high precision so this will not burn.
+    float4 source_pix = to_linear(source_texture.Load(dtid));
+    float4 new_pix = dest_texture[dtid.xy] + source_pix * mosample_weight;
+    dest_texture[dtid.xy] = new_pix;
 }
